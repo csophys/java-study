3a33356729a2e80c280e99d7cd7d4c52f8795ba2
csophys <zjsy_cs@163.com>
2016-04-19 23:02:54 +0800

    nio socket demo ok

--- src/main/java/books/deep_analysis_java_web/IO/NIO.java
+++ src/main/java/books/deep_analysis_java_web/IO/NIO.java
@@ -1,14 +1,20 @@
 package books.deep_analysis_java_web.IO;
 
+import com.jayway.awaitility.Awaitility;
+import com.jayway.awaitility.Duration;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
+import java.nio.channels.*;
 import java.nio.charset.Charset;
+import java.util.Set;
+import java.util.concurrent.Callable;
 
 /**
  * Created by csophys on 16/4/16.
@@ -66,7 +72,57 @@ public class NIO {
     }
 
     @Test
-    public void SocketNio() {
-        //TODO
+    public void SocketNio() throws IOException {
+        // --- server socket
+        //新建一个serverSocket
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        //绑定监听端口
+        serverSocketChannel.bind(new InetSocketAddress(1999));
+
+        //非阻塞模式
+        serverSocketChannel.configureBlocking(false);
+
+
+        //创建 selector
+        final Selector selector = Selector.open();
+
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        // --- client socket1
+
+        SocketChannel socketChannel = SocketChannel.open();
+        socketChannel.connect(new InetSocketAddress(1999));
+
+
+        // 可以继续设置多个socket
+
+        Awaitility.await().atMost(Duration.FIVE_SECONDS).until(new Callable<Boolean>() {
+            @Override
+            public Boolean call() throws Exception {
+                while (true) {
+                    /**
+                     * 等待客户端连接；
+                     */
+                    selector.select();
+
+                    //处理连接的请求
+                    Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                    for (SelectionKey selectionKey : selectionKeys) {
+                        ServerSocketChannel serverSocketChannel1 = (ServerSocketChannel) selectionKey.channel();
+                        //获取channel
+                        SocketChannel socketChannel2 = serverSocketChannel1.accept();
+                        // 设置成非阻塞
+                        socketChannel2.configureBlocking(false);
+                        //更改channel的事件类型
+                        socketChannel2.register(selector, SelectionKey.OP_READ);
+                    }
+                    System.out.println(selectionKeys);
+                    return true;
+                }
+
+            }
+        }, Matchers.equalTo(true));
+
+
     }
 }
