ce888a788388dcb287755679664d3f0c66d40b86
陈胜 <csophys@chenshengdeMacBook-Pro.local>
2016-06-26 22:17:36 +0800

    java observer design pattern

--- src/main/java/base/javabeans/PropertyChangeTarget.java
+++ src/main/java/base/javabeans/PropertyChangeTarget.java
@@ -7,35 +7,28 @@ import java.beans.PropertyChangeSupport;
 /**
  * Created by csophys on 16/2/3.
  */
+
+/**
+ * 被观察者
+ */
 public class PropertyChangeTarget {
 
-    PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
-
-    public void addListener(PropertyChangeListener listener) {
-        propertyChangeSupport.addPropertyChangeListener(listener);
-    }
-
-    public void removeListener(PropertyChangeListener listener) {
-        propertyChangeSupport.removePropertyChangeListener(listener);
-    }
+    public static PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(new PropertyChangeTarget());
 
     public static void main(String[] args) {
-        PropertyChangeTarget propertyChangeFirer = new PropertyChangeTarget();
-        propertyChangeFirer.addListener(new PropertyChangeListener() {
-            @Override
+        PropertyChangeTarget.propertyChangeSupport.addPropertyChangeListener(new PropertyChangeListener() {
             public void propertyChange(PropertyChangeEvent evt) {
-                System.out.println("first Listener receive the evt" + evt);
-            }
-        });
-        propertyChangeFirer.addListener(new PropertyChangeListener() {
-            @Override
-            public void propertyChange(PropertyChangeEvent evt) {
-                System.out.println("last Listener receive the evt" + evt);
+                System.out.println(evt+"I am first observer");
             }
         });
 
-        propertyChangeFirer.propertyChangeSupport.firePropertyChange(new PropertyChangeEvent("source", "name", "cs", "csophys"));
+        PropertyChangeTarget.propertyChangeSupport.addPropertyChangeListener(new PropertyChangeListener() {
+            public void propertyChange(PropertyChangeEvent evt) {
+                System.out.println(evt + " I am second observer");
+            }
+        });
 
+        PropertyChangeTarget.propertyChangeSupport.firePropertyChange("name","csophys", "chen sheng");
     }
 
 }
