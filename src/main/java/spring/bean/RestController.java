c04b1d29bd4c36556205acbf8bcd5c074226fdb9
陈胜 <csophys@chenshengdeMacBook-Pro.local>
2016-07-09 14:49:51 +0800

    normal SpringMvcDto and RequestBodyDto

--- src/main/java/spring/bean/RestController.java
+++ src/main/java/spring/bean/RestController.java
@@ -1,5 +1,7 @@
 package spring.bean;
 
+import lombok.Data;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -13,4 +15,31 @@ public class RestController {
     public Model simpleRest() {
         return new Model("csophys", "男");
     }
+
+
+    @RequestMapping("/normal")
+    public String normalDTO(TestDTO testDTO){
+        return testDTO.toString();
+    }
+
+    @RequestMapping("/requestBody")
+    public String requestBody(@RequestBody TestDTO testDTO){
+        return testDTO.toString();
+    }
+
 }
+
+@Data
+class TestDTO{
+    private String name;
+    private String sex;
+
+    @Override
+    public String toString() {
+        return "TestDTO{" +
+                "name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
+
