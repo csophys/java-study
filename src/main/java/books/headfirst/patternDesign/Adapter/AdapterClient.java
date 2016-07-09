8e8e8026ec79e8875219af2a0d0c737ccec389fa
陈胜 <csophys@chenshengdeMacBook-Pro.local>
2016-07-09 22:27:01 +0800

    rename direcotry

--- src/main/java/books/headfirst/patternDesign/Adapter/AdapterClient.java
+++
@@ -1,7 +0,0 @@
-package books.headfirst.patternDesign.Adapter;
-
-/**
- * Created by csophys on 16/7/9.
- */
-public class AdapterClient {
-}
--- src/main/java/books/headfirst/patternDesign/adapter/AdapterClient.java
+++ src/main/java/books/headfirst/patternDesign/adapter/AdapterClient.java
@@ -1,19 +1,15 @@
 package books.headfirst.patternDesign.Adapter;
 
-import org.junit.Test;
-
 /**
  * Created by csophys on 16/7/9.
  */
-public class AdapterClientTest {
+public class AdapterClient {
 
-    @Test
-    public void AdapterClientTest(){
+    public void testAdapter(){
         //实例化被适配类
         Adaptee adaptee = new AdapteeImpl();
         //实例化适配器
         Target targetAdapter = new TargetAdapter(adaptee);
         targetAdapter.execute(1,2);
     }
-
 }
