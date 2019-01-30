02c48bd4bb280e040728b3db0c603b4633111cc6
sheng.chen@dianping.com <zjsy_cs@163.com>
2019-01-30 17:46:45 +0800

    add generic TYpe

---
+++ src/main/java/base/generic/GetGenericInfo.java
@@ -0,0 +1,20 @@
+package base.generic;
+
+import java.lang.reflect.Type;
+
+public class GetGenericInfo extends Hello<String,Integer> {
+
+    public static void main(String[] args) {
+
+        Type genericSuperclass = GetGenericInfo.class.getGenericSuperclass();
+        System.out.println(genericSuperclass);
+    }
+}
+
+
+class Hello<T,S> {
+
+}
+
+
+
