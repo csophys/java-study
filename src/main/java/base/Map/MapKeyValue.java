1e45e0219cfa89cb741ee676d4fa11db8f157227
sheng.chen@dianping.com <zjsy_cs@163.com>
2019-06-21 09:23:59 +0800

    add javaShift

--- src/main/java/base/Map/MapKeyValue.java
+++ src/main/java/base/Map/MapKeyValue.java
@@ -3,8 +3,8 @@ package base.Map;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by csophys on 16/4/13.
@@ -13,12 +13,12 @@ public class MapKeyValue {
 
     @Test
     public void testGetKey(){
-        Map map = new HashMap<String,String>();
+        Map map = new ConcurrentHashMap();
 
+        System.out.println(map.containsKey(null));
         map.put("1",null);
         Assert.assertEquals(map.get("1"),null);
         map.put(null,null);
         Assert.assertEquals(map.get(null),null);
-
     }
 }
---
+++ src/main/java/base/base/JavaShift.java
@@ -0,0 +1,18 @@
+package base.base;
+
+public class JavaShift {
+
+    public static void main(String[] args) {
+        System.out.println(4 << 5);
+        System.out.println(4 << 28);
+        System.out.println(4 << 29);
+        System.out.println(4 << 31);
+        System.out.println(4 << 32);
+        System.out.println(4 << 33);
+        System.out.println(4 << 34);
+        System.out.println(4 << 35);
+        System.out.println(4 << 37);
+        System.out.println(4 << 101);
+        System.out.println(4L << 101);
+    }
+}
