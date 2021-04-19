43cb3e676cd4c674975e36fbda53fa717e23ebdd
sheng.chen@dianping.com <zjsy_cs@163.com>
2021-04-19 08:17:00 +0800

    三指针的反转方法经典

--- src/main/java/books/geek/datastructure_algorithm/LinkedList.java
+++ src/main/java/books/geek/datastructure_algorithm/LinkedList.java
@@ -51,11 +51,12 @@ public class LinkedList {
         LinkNode node4 = new LinkNode(4);
         LinkNode node5 = new LinkNode(5);
 
-        head.next=node1;
-        node1.next=node2;
-        node2.next=node3;
-        node3.next=node4;
-        node4.next=node5;
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node5.next = null;
     }
 
     @Test
@@ -87,30 +88,85 @@ public class LinkedList {
     }
 
     @Test
-    public void testGetNTailNode(){
-        Assert.assertEquals(2,getTailNLinkNode(4).val);
-        Assert.assertEquals(4,getTailNLinkNode(2).val);
-        Assert.assertEquals(1,getTailNLinkNode(5).val);
-        Assert.assertEquals(5,getTailNLinkNode(1).val);
+    public void testGetNTailNode() {
+        Assert.assertEquals(2, getTailNLinkNode(4).val);
+        Assert.assertEquals(4, getTailNLinkNode(2).val);
+        Assert.assertEquals(1, getTailNLinkNode(5).val);
+        Assert.assertEquals(5, getTailNLinkNode(1).val);
     }
 
     @Nullable
     private LinkNode getTailNLinkNode(int n) {
-        LinkNode fast=head,slow=head;
+        LinkNode fast = head, slow = head;
         //fast先走n步。然后fast和slow开始一起前进，直到fast为null
         //fast先走n步
-        for(int i = 0; i< n; i++){
-            if(fast==null) {
+        for (int i = 0; i < n; i++) {
+            if (fast == null) {
                 return null;
-            }else {
+            } else {
                 fast = fast.next;
             }
         }
         //一起前进
-        while(fast!=null){
-            fast=fast.next;
-            slow=slow.next;
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
         }
         return slow;
     }
+
+    @Test
+    public void testCircle() {
+        LinkNode current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        //current为尾结点
+        //构造了存在环的一个链表。
+        current.next = head.next;
+        //设置快慢指针，如果快慢指针相遇，判断存在环。
+        LinkNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                //存在环
+                System.out.println(true);
+                break;
+            }
+        }
+    }
+
+    @Test
+    public void reverseLink() {
+        //单链表反转的三个方法
+        //1.创建新的链表，然后头插法到新链表中。
+        //2.就地反转，针对原来的链表，通过头插法插入。
+        //3.三指针方法。遍历链表。更换链表指针方向。
+        //4.递归实现
+
+        init();
+        //定义三个指针。
+        LinkNode p=null, current, q;
+        if (head == null || head.next == null) return;
+        current =head;
+        //判断当当前结点，当前节点不为空，就开始变换指针位置
+        while (current != null) {
+            //q是需要迭代的时候才赋值出来的。current肯定不是null
+            q= current.next;
+            //链表指向反转
+            current.next=p;
+            //开始下一个迭代赋值
+            p=current;
+            current=q;
+        }
+        //重新打印新链表
+        LinkNode cur = p;
+        while (cur != null) {
+            System.out.println(cur.val);
+            cur = cur.next;
+        }
+
+    }
+
 }
